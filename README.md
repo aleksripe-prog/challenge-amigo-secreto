abc471a2ba0edb517c5de3447c55f02526e9b2bd
aleksripe-prog <aleksripe@gmail.com>
2025-09-15 03:41:09 -0600

    se creo la funcion usuarioAgregar para poder verificar si la lista esta vacia antes de agregar los nombres

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # challenge-amigo-secreto
+# Actividad del curso de alura next for education oracle 
+# Autor Jose Alejandro rivera perez 
+# 9 de septiembre del 2025
+# 
--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables
+let usuario = []
+let UsuarioNombre;
+
+/**
+ * funcion que valida que el nombre del usuario no este vacio antes de agregarlo a la lista 
+ * 
+ */
+function usuarioAgregar(){
+    UsuarioNombre = document.getElementById("amigo").value;
+    if (UsuarioNombre.trim() ==="") {
+        alert("el campo esta vacio agrega un nombre");
+    } else {
+        usuario.push(UsuarioNombre);
+        document.querySelector("#amigo").value = "";
+        //funcion para mostrar la lista de los amigos
+    }
+}
+
