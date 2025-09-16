f0651ffb6de5e0ffb9f1d604b3205f9788a11acb
aleksripe-prog <aleksripe@gmail.com>
2025-09-15 23:26:30 -0600

    modificacion de los erores de referencia de los nombres de las funciones

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //variables
 let usuario = []
-let UsuarioNombre;
-let usuarioListaNombre;
-let cantidadNombres;
+//let UsuarioNombre;
+//let usuarioListaNombre;
+//let cantidadNombres;
 /**
  * funcion que valida que el nombre del usuario no este vacio antes de agregarlo a la lista 
  * 
  */
 function usuarioAgregar(){
-    UsuarioNombre = document.getElementById("amigo").value;
+    let UsuarioNombre = document.getElementById("amigo").value;
 
     if (UsuarioNombre.trim() ==="") {
         alert("el campo esta vacio agrega un nombre");
     } else {
         usuario.push(UsuarioNombre);
         document.querySelector("#amigo").value = "";
+        usuarioListaMostrar();
         //funcion para mostrar la lista de los amigos
     }
 }
@@ -23,16 +24,16 @@ function usuarioAgregar(){
  * funcion para actualizar la lista de amigos en el DOM, crea elementos "li" por cada usuarios
  * 
  */
-function usuarioLista(){
-    usuarioListaNombre = document.querySelector("#listaAmigos");
-    listaAmigos.innerHTML = "";
+function usuarioListaMostrar(){
+    let usuarioListaNombre = document.querySelector("#listaAmigos");
+    usuarioListaNombre.innerHTML = "";
 
-    for(let index = 0; index<amigos.length; index++){
-        const element = amigos[index];
+    for(let index = 0; index<usuario.length; index++){
+        const element = usuario[index];
 
         let listaHTML = document.createElement("li");
         listaHTML.textContent = element;
-        listaAmigos.appendChild(listaHTML);
+        usuarioListaNombre.appendChild(listaHTML);
     }
 }
 /**
@@ -41,12 +42,12 @@ function usuarioLista(){
  * 
  */
 function sortearUsuario() {
-  cantidadNombres = amigos.length;
+ let cantidadNombres = amigos.length;
 
-  if (cantidadAmigos === 0) {
+  if (cantidadNombres === 0) {
     alert("Por favor, inserte un nombre antes de sortear");
   } else {
-    let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+    let indiceAmigo = Math.floor(Math.random() * cantidadNombres);
     let resultadoHTML = document.querySelector("#resultado");
     resultadoHTML.innerHTML = amigos[indiceAmigo];
   }
